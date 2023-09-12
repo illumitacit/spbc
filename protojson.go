38721a6d2981829aa8b8e1163ae55673396c1323
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-09-12 07:22:11 -0700

    Add a JSONB backed protobuf object (#1)
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

---
+++ protojson.go
@@ -0,0 +1,113 @@
+package spbc
+
+import (
+	"bytes"
+	"database/sql"
+	"database/sql/driver"
+	"encoding"
+	"encoding/json"
+	"fmt"
+	"reflect"
+
+	"github.com/volatiletech/null/v8"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// PBJSON represents a protojson message of a specific type, controlled by the generic type parameter.
+//
+// Note that this handles NULL in postgres separately from an empty protobuf object. A NULL column value will be
+// represented with a false value for the Valid field.
+type PBJSON[T proto.Message] struct {
+	Object T
+	Valid  bool
+}
+
+// Assert that expected interfaces are implemented
+var _ (json.Marshaler) = (*PBO[*emptypb.Empty])(nil)
+var _ (json.Unmarshaler) = (*PBO[*emptypb.Empty])(nil)
+var _ (encoding.TextMarshaler) = (*PBO[*emptypb.Empty])(nil)
+var _ (encoding.TextUnmarshaler) = (*PBO[*emptypb.Empty])(nil)
+var _ (driver.Valuer) = (*PBO[*emptypb.Empty])(nil)
+var _ (sql.Scanner) = (*PBO[*emptypb.Empty])(nil)
+
+// NewPBJSON creates a new PBJSON object.
+func NewPBJSON[T proto.Message](o T, valid bool) PBO[T] {
+	return PBO[T]{
+		Object: o,
+		Valid:  valid,
+	}
+}
+
+// PBJSONFrom creates a new PBJSON from a valid protobuf object.
+//
+// Note that proto.Message is inherently a pointer to a protobuf object and thus can be nil, but golang's type system
+// isn't smart enough to allow nil checks against the generic type since it can't guarantee it's a pointer. As such,
+// we have to resort to a lousy reflect check.
+func PBJSONFrom[T proto.Message](o T) PBO[T] {
+	return NewPBJSON(o, !reflect.ValueOf(o).IsNil())
+}
+
+// IsValid returns true if this carries an explicit value.
+func (o PBJSON[T]) IsValid() bool {
+	return o.Valid
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (o *PBJSON[T]) UnmarshalJSON(data []byte) error {
+	if bytes.Equal(data, null.NullBytes) {
+		o.Valid = false
+		return nil
+	}
+
+	// If the object is nil, generate a new empty object to receive the unmarshalled data.
+	if reflect.ValueOf(o.Object).IsNil() {
+		o.Object = o.Object.ProtoReflect().New().Interface().(T)
+	}
+
+	if err := protojson.Unmarshal(data, o.Object); err != nil {
+		return err
+	}
+	o.Valid = true
+	return nil
+}
+
+// UnmarshalText implements encoding.TextUnmarshaler.
+func (o *PBJSON[T]) UnmarshalText(text []byte) error {
+	return o.UnmarshalJSON(text)
+}
+
+// MarshalJSON implements json.Marshaler.
+func (o PBJSON[T]) MarshalJSON() ([]byte, error) {
+	if !o.Valid {
+		return null.NullBytes, nil
+	}
+
+	return protojson.Marshal(o.Object)
+}
+
+// MarshalText implements encoding.TextMarshaler.
+func (o PBJSON[T]) MarshalText() ([]byte, error) {
+	return o.MarshalJSON()
+}
+
+// Scan implements the sql.Scanner interface.
+func (o *PBJSON[T]) Scan(value interface{}) error {
+	if value == nil {
+		o.Valid = false
+		return nil
+	}
+
+	bytes, isBytes := value.([]byte)
+	if !isBytes {
+		return fmt.Errorf("unsupported Scan, storing driver.Value type %T into type PBO", value)
+	}
+
+	return o.UnmarshalJSON(bytes)
+}
+
+// Value implements the driver.Valuer interface.
+func (o PBJSON[T]) Value() (driver.Value, error) {
+	return o.MarshalJSON()
+}
---
+++ protojson_test.go
@@ -0,0 +1,83 @@
+package spbc
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestPBJSONFromValidObj(t *testing.T) {
+	o := PBJSONFrom(&_emptyObj)
+	assert.True(t, proto.Equal(o.Object, &_emptyObj))
+}
+
+func TestPBJSONFromNilObj(t *testing.T) {
+	nilObj := PBJSONFrom[*emptypb.Empty](nil)
+	assert.False(t, nilObj.Valid, "expected nil PBJSON to be invalid")
+}
+
+func TestUnmarshalPBJSONValid(t *testing.T) {
+	var o PBJSON[*emptypb.Empty]
+	err := json.Unmarshal([]byte("{}"), &o)
+	require.NoError(t, err)
+	assert.True(t, proto.Equal(o.Object, &_emptyObj))
+}
+
+func TestUnmarshalPBJSONInvalid(t *testing.T) {
+	var o PBJSON[*emptypb.Empty]
+	err := json.Unmarshal([]byte("56"), &o)
+	require.Error(t, err)
+}
+
+func TestTextUnmarshalPBJSONValid(t *testing.T) {
+	var o PBJSON[*emptypb.Empty]
+	err := o.UnmarshalText([]byte("{}"))
+	require.NoError(t, err)
+	assert.True(t, proto.Equal(o.Object, &_emptyObj))
+}
+
+func TestTextUnmarshalPBJSONInvalid(t *testing.T) {
+	var o PBJSON[*emptypb.Empty]
+	err := o.UnmarshalText([]byte("42"))
+	require.Error(t, err)
+}
+
+func TestMarshalPBJSONValid(t *testing.T) {
+	o := PBJSONFrom(&_emptyObj)
+	data, err := json.Marshal(o)
+	require.NoError(t, err)
+	assert.Equal(t, "{}", string(data))
+}
+
+func TestMarshalPBJSONInvalid(t *testing.T) {
+	// invalid values should be encoded as null
+	null := NewPBJSON[*emptypb.Empty](nil, false)
+	data, err := json.Marshal(null)
+	require.NoError(t, err)
+	assert.Equal(t, "null", string(data))
+}
+
+func TestMarshalPBJSONText(t *testing.T) {
+	o := PBJSONFrom(&_emptyObj)
+	data, err := o.MarshalText()
+	require.NoError(t, err)
+	assert.Equal(t, "", string(data))
+}
+
+func TestPBJSONScanValid(t *testing.T) {
+	var o PBJSON[*emptypb.Empty]
+	err := o.Scan([]byte("{}"))
+	require.NoError(t, err)
+	assert.True(t, proto.Equal(o.Object, &_emptyObj))
+}
+
+func TestPBJSONScanNull(t *testing.T) {
+	var null PBJSON[*emptypb.Empty]
+	err := null.Scan(nil)
+	require.NoError(t, err)
+	assert.False(t, null.Valid)
+}
